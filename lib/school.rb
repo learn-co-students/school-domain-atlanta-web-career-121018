19bb4ee59d090978165646e324c88f630b318493
Shivang Dave <shivang.dave@flatironschool.com>
2019-05-09 15:25:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School 
+  
+    attr_accessor :name, :grade, :roster
+    
+    def initialize(name, roster={})
+      @name = name
+      @roster = roster
+    end
+    
+    def roster
+      @roster
+    end
+    
+    def add_student(name,grade)
+      @roster[grade] ||= []
+      @roster[grade] << name
+    end
+    
+    def grade(grade)
+      @roster[grade]
+    end
+    
+    def sort
+      @roster.map{|key,names| [key, names.sort]}.sort.to_h
+    end
+end
